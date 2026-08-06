281641682f13c32c1f048ad8ca4689769cac7890
Haixing Hu <starfish.hu@gmail.com>
2026-08-06 13:14:00 +0800

    test: remove duplicate integration test modules

--- tests/mod.rs
+++ tests/mod.rs
@@ -8,7 +8,6 @@
 //! Integration tests for `qubit-id`.
 
 mod benchmark_tests;
-mod generation_attempt_tests;
 mod markdown_tests;
 #[cfg(any(
     feature = "qubit-snowflake",
@@ -22,6 +21,5 @@ mod snowflake;
     feature = "sonyflake",
 ))]
 mod support;
-mod try_id_generator_tests;
 #[cfg(feature = "uuid")]
 mod uuid;
